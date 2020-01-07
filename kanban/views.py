ef245fd1915bd75e824be0ea6ec4afc69c8b5676
ec0341ks <idc68730@gmail.com>
2020-01-07 21:29:53 +0900

    サインアップ時のビュー作成

--- kanban/views.py
+++ kanban/views.py
@@ -8,3 +8,18 @@ def index(request):
 
 def home(request):
     return render(request, 'kanban/home.html')
+
+def signup(request):
+    if request.method == 'POST':        # POSTする際の挙動書く
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user_instance = form.save()
+            login(request, user_instance)
+            return redirect('kanban:home')
+
+    else:                               # GETする際の挙動書く
+        form = UserCreationForm()
+    context = {
+            "form": form
+    }
+    return render(request, 'kanban/signup.html', context)
