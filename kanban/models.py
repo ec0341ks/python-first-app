d3a0851a08069da11139bbd913ef2ab22f7b7026
ec0341ks <idc68730@gmail.com>
2020-01-22 16:02:51 +0900

    Listモデルのマイグレーション実行

--- kanban/models.py
+++ kanban/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class List(models.Model):
+    title = models.CharField(max_length=200)
+    # on_deleteは、ユーザー削除時にリストも同時に削除する動作
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
